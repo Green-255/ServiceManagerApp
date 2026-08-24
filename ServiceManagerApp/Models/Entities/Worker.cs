7b110bdd5edbf3eead1bd6441ed7198c73cdd586
Green-255 <naglis.kontautas04@gmail.com>
2026-08-24 15:07:28 +0300

    refactor(Worker): establish business side and it's workers

--- ServiceManagerApp/Models/Entities/Worker.cs
+++ ServiceManagerApp/Models/Entities/Worker.cs
@@ -7,10 +7,13 @@ namespace ServiceManagerApp.Models.Entities
         public int Id { get; set; }
         public string ReferenceCode { get; set; } = string.Empty; // unique code for each worker, maybe use a GUID or a combination of name and ID
         public string Name { get; set; } = string.Empty;
-        public AvailabilityStatus AvailabilityStatus = AvailabilityStatus.Available;
+        public AvailabilityStatus AvailabilityStatus { get; set; } = AvailabilityStatus.Available;
         public int? PhoneNumber { get; set; }
-        public string Email { get; set; } = string.Empty;
-        public WorkSector WorkSector { get; set; } = WorkSector.Unsigned;
-        public Occupation Occupation { get; set; } = Occupation.Unsigned; // prob need different enums for each WorkSector. Maybe use func?
+        public string Email { get; set; } = null!;
+        public int? DepartamentId { get; set; }
+        public Departament? Departament { get; set; }
+        public int? JobRoleId { get; set; }
+        public JobRole? JobRole { get; set; }
+        public ICollection<Service> Services { get; set; } = [];
     }
 }
