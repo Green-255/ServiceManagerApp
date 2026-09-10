90f685abd38ae071737d1e26274051d49b04a262
nei nei <naglis.kontautas04@gmail.com>
2026-09-10 13:12:11 +0300

    fix(Department): fix foreign key in JobRoles error when deleting Department

---
+++ ServiceManagerApp/Data/Migrations/20260910100835_JobRoleDepartmentSetToNullOnDelete.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceManagerApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class JobRoleDepartmentSetToNullOnDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobRoles_Departments_DepartmentId",
+                table: "JobRoles");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId1",
+                table: "JobRoles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobRoles_DepartmentId1",
+                table: "JobRoles",
+                column: "DepartmentId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobRoles_Departments_DepartmentId",
+                table: "JobRoles",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobRoles_Departments_DepartmentId1",
+                table: "JobRoles",
+                column: "DepartmentId1",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobRoles_Departments_DepartmentId",
+                table: "JobRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobRoles_Departments_DepartmentId1",
+                table: "JobRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JobRoles_DepartmentId1",
+                table: "JobRoles");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId1",
+                table: "JobRoles");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobRoles_Departments_DepartmentId",
+                table: "JobRoles",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+    }
+}
